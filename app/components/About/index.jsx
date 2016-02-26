b0f91f5b42f72504869bf8c755d5d37d4f51d1f0
looklikeapro <jerryzhou00@gmail.com>
2016-02-26 02:41:14 -0500

    correct translations

--- app/components/About/index.jsx
+++ app/components/About/index.jsx
@@ -9,14 +9,14 @@ module.exports = React.createClass({
 		return <div className='aboutPage pageContent'>
 			<h1>About</h1>
 			<p>The NFC SmartBook Project is a pilot project at Renison University College incorporates NFC technology with ebooks to create a mobile e-library browsing experience.</p>
-			<p>By attaching a NFC card to a library book, one can tap the book with a NFC enabled device to conveniently browse online resources related to the book. In the case of Deborah Dejong's book <Link to={`/book/dance`}>D.A.N.C.E.: five steps to Living Your Best Life</Link>, the mobile friendly site provides information about the book, digital reading options, and exclusive interviews with fans.</p>
+			<p>By attaching a NFC card to a library book, one can tap the book with a NFC enabled device to conveniently browse online resources related to the book. In the case of Deborah DeJong's book <Link to={`/book/dance`}>D.A.N.C.E.: five steps to Living Your Best Life</Link>, the mobile friendly site provides information about the book, digital reading options, and exclusive interviews with fans.</p>
 			<h3>Results</h3>
 			<p>The project has received mention on the book <a href="http://www.amazon.ca/Near-Field-Communication-Developments-Implications/dp/1627054324/ref=sr_1_2?ie=UTF8&qid=1420646410&sr=8-2&keywords=Near+Field%3A+recent+development">Near Field Communication: Recent Developments and Library Implications</a>.</p>
 			<h3>Contributors</h3>
 			<List>
 				<ListItem><b>Project Director:</b> Tony Tin</ListItem>
 				<ListItem><b>Project Lead:</b> Jerry Zhou</ListItem>
-				<ListItem><b>Author:</b> Deborah Dejong</ListItem>
+				<ListItem><b>Author:</b> Deborah DeJong</ListItem>
 			</List>
 			<List subheader="Pilot Members">
 				<ListItem primaryText="Carrie Wang" />
@@ -24,7 +24,7 @@ module.exports = React.createClass({
 				<ListItem primaryText="Vivi Ma" />
 				<ListItem primaryText="Linda Yan" />
 			</List>
-			<p>Deborah Dejong inspires everyone whose life she touches. She invites the reader to live their best life everyday! Dejong demonstrates through her five step model, that happiness and peace are available to everyone if they choose to DANCE.</p>
+			<p>Deborah DeJong inspires everyone whose life she touches. She invites the reader to live their best life everyday! DeJong demonstrates through her five step model, that happiness and peace are available to everyone if they choose to DANCE.</p>
 			<h3>Contact</h3>
 			<List>
 				<ListItem primaryText="Email" secondaryText="tony.tin@uwaterloo.ca" />
