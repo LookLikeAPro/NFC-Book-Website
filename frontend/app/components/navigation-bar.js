213349121e24a9275b6be43809d77f3b035d0f89
looklikeapro <jerryzhou00@gmail.com>
2016-09-07 02:37:34 -0400

    delete old nav bar

--- frontend/app/components/navigation-bar.js
+++ frontend/app/components/navigation-bar.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	expanded: false,
+	class: Ember.computed(function() {
+		var css = this.get("expanded")? "nav-bar-to" : "nav-bar-top";
+		return css;
+	}).property("expanded"),
+	actions: {
+		click: function() {
+			this.set("expanded", !this.get("expanded"));
+		}
+	}
 });
